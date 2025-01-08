91d9da613233d6e0399341f0b667f053f943ccd1
Alaa Zorkane <alaazorkane@gmail.com>
2025-01-08 16:00:16 +0100

    feat: phantom key converter

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-# Turbin3 Typecript Prereq
+<img src="https://turbin3.com/turbine-logo-text.svg" alt="Turbin3" width="200">
+
+**Turbin3 Typescript Prereq - Anchor Version**
+
+## What do you mean by "anchor version"?
+I had some time after finishing up this version (which follows exactly the instructions in the prereq document) and I was looking for something to learn, so I made a codama and web3js v2 version [here](https://github.com/AlaaZorkane/turbin3-ts-prereq-v2).
+
+## Structure
+- `phantom.ts`: Phantom key converter, helps you convert from/to base58 encoded secret keys, `prompt-sync` is wonky though.
+- `keygen.ts`: Keygen, simple script to generate a `dev-wallet.json`, it doesn't overwrite the file if it already exists
+- `airdrop.ts`: Airdrop, simple script to airdrop SOL to your `dev-wallet.json`
+- `transfer.ts`: The transfer script, it uses the `dev-wallet.json` to transfer SOL to the specified address and later to drain the wallet to turbin3's address.
+- `enroll.ts`: Uses Anchor's IDL to call the confirm function with my github username.
 
 ### Setup
 
@@ -24,6 +36,8 @@ pnpm airdrop
 ```
 
 ### Transfer
+
+CAUTION: this drains the wallet, so maybe call `enroll` first to prevent airdrop 429.
 
 ```bash
 pnpm transfer
--- phantom.ts
+++ phantom.ts
@@ -1,17 +1,44 @@
-import { Keypair } from "@solana/web3.js";
-import fs from "node:fs";
 import bs58 from "bs58";
+import { Keypair } from "@solana/web3.js";
 
-try {
-  const file = fs.readFileSync("dev-wallet.json", "utf8");
-
-  const wallet = JSON.parse(file);
-  const keypair = Keypair.fromSecretKey(Uint8Array.from(wallet));
-  console.log("public key (base58):", keypair.publicKey.toBase58());
-  console.log("private key (base58):", bs58.encode(keypair.secretKey));
-} catch {
-  console.error(
-    "Failed to parse wallet. Please run `pnpm keygen:export` to generate a wallet.",
+(() => {
+  console.info(
+    "Small utility to help you convert from/to base58 encoded secret keys.",
   );
-  process.exit(1);
-}
+
+  // const input = promptSync();
+
+  // const raw = input(
+  //   "[+] Please enter your private key (in base58 or array uint8): \n> ",
+  // );
+  // const raw =
+  //   "[58,0,140,60,7,56,25,209,243,59,213,164,86,244,146,209,155,37,250,122,217,226,179,97,172,56,135,86,245,19,16,108,79,107,133,90,255,115,218,156,255,163,157,65,194,82,89,33,173,26,29,225,230,5,154,25,33,127,53,79,221,26,96,35]";
+  const raw =
+    "2AG4MDMsHNV5vU9a6VsGQG6j4kRPQu7HQ82pMDYow8G1modZ5VTCuUAPP83QDSgRPXHoEF3z5mrbRJJu6HTXSmsQ";
+
+  if (raw.startsWith("[")) {
+    const uint8Array = new Uint8Array(JSON.parse(raw));
+    const keypair = Keypair.fromSecretKey(uint8Array);
+    const privateKey = uint8Array.slice(0, 32);
+    const publicKeyBs58 = bs58.encode(keypair.publicKey.toBuffer());
+    const privateKeyBs58 = bs58.encode(privateKey);
+    const secretKeyBs58 = bs58.encode(keypair.secretKey);
+
+    console.info("Public Key (bs58): %s", publicKeyBs58);
+    console.info("Secret Key (phantom): %s", secretKeyBs58);
+    console.info("Private Key (bs58): %s", privateKeyBs58);
+    console.info("Secret key (uint8[]): %s", Array.from(uint8Array));
+  } else {
+    const uint8Array = bs58.decode(raw);
+    const keypair = Keypair.fromSecretKey(uint8Array);
+    const privateKey = uint8Array.slice(0, 32);
+    const publicKeyBs58 = bs58.encode(keypair.publicKey.toBuffer());
+    const privateKeyBs58 = bs58.encode(privateKey);
+    const secretKeyBs58 = bs58.encode(keypair.secretKey);
+
+    console.info("Public Key (bs58): %s", publicKeyBs58);
+    console.info("Secret Key (phantom): %s", secretKeyBs58);
+    console.info("Private Key (bs58): %s", privateKeyBs58);
+    console.info("Secret key (uint8[]): %s", Array.from(uint8Array));
+  }
+})();
